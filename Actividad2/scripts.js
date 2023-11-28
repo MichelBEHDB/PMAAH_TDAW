3386be41fa0f44b6be1a5358e81b5a80499a0695
MichelBEHDB <145080519+MichelBEHDB@users.noreply.github.com>
2023-11-27 22:10:32 -0600

    Add files via upload

--- Actividad2/scripts.js
+++ Actividad2/scripts.js
@@ -2,6 +2,10 @@
 const piezasRompecabezas = document.querySelectorAll('.pieza-rompecabezas');
 const botonIniciar = document.getElementById('boton-iniciar');
 
+// Para el tiempo
+const tiempoTranscurridoElemento = document.getElementById('tiempo-transcurrido');
+let tiempoInicio;
+
 // Función para verificar si el rompecabezas está resuelto
 function esRompecabezasResuelto() {
   for (let i = 0; i < piezasRompecabezas.length - 1; i++) {
@@ -12,17 +16,27 @@ function esRompecabezasResuelto() {
   return true;
 }
 
+// Funcion para desordenar las piezas (arreglo)
+function desordenar(array) {
+  const longitud = array.length;
+
+  for (let i = longitud - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+
+    // Intercambiar elementos
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
+
 // Función para iniciar el juego
 function iniciarJuego() {
   // Crear una matriz de valores del 1 al 15
   const valores = Array.from({ length: 15 }, (_, i) => i + 1);
   valores.push(0); // Agregar un 0 para representar la casilla en blanco
 
-  // Desordenar los valores
-  for (let i = valores.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [valores[i], valores[j]] = [valores[j], valores[i]];
-  }
+  desordenar(valores);
 
   // Asignar los valores desordenados a las piezas del rompecabezas
   piezasRompecabezas.forEach((pieza, index) => {
@@ -37,8 +51,25 @@ function iniciarJuego() {
 
 // Manejador de clic en el botón de inicio
 botonIniciar.addEventListener('click', () => {
+  // Empieza a contar el tiempo
+  tiempoInicio = new Date();
   iniciarJuego();
+  actualizarTiempo();
 });
+
+function actualizarTiempo() {
+  // Calcula el tiempo transcurrido en milisegundos
+  const tiempoTranscurrido = new Date() - tiempoInicio;
+
+  // Convertir el tiempo a segundos
+  const segundosTranscurridos = Math.floor(tiempoTranscurrido / 1000);
+
+  // Actualiza el elemento HTML con el tiempo transcurrido
+  tiempoTranscurridoElemento.textContent = `Tiempo: ${segundosTranscurridos} segundos`;
+
+  // para que sea en 1 segundo
+  setTimeout(actualizarTiempo, 1000);
+}
 
 let movimientos = 0;
 
@@ -49,9 +80,12 @@ piezasRompecabezas.forEach((pieza) => {
       const posicionActual = Array.from(piezasRompecabezas).indexOf(pieza);
       const posicionVacia = Array.from(piezasRompecabezas).findIndex((pieza) => pieza.classList.contains('pieza-vacia'));
 
+      // Verificar las posiciones de columna y fila de la ficha vacia y ciclada 
       const diferenciaFila = Math.floor(posicionActual / 4) - Math.floor(posicionVacia / 4);
       const diferenciaColumna = posicionActual % 4 - posicionVacia % 4;
 
+      // Validar que el movimiento es valido, osea que las piezas sen adyacentes a a la pieza vacia  para poder hace el movimiento
+      //  se intercambian las clases y los textos entre la pieza clicada y la pieza vacía.
       if ((Math.abs(diferenciaFila) === 1 && diferenciaColumna === 0) || (Math.abs(diferenciaColumna) === 1 && diferenciaFila === 0)) {
         pieza.classList.toggle('pieza-vacia');
         piezasRompecabezas[posicionVacia].classList.toggle('pieza-vacia');
@@ -66,6 +100,14 @@ piezasRompecabezas.forEach((pieza) => {
     }
 
     if (esRompecabezasResuelto()) {
+      tiempoFin = new Date();
+
+      // Calcula la diferencia de tiempo en milisegundos
+      const tiempoTranscurrido = tiempoFin - tiempoInicio;
+
+      // Convierte la diferencia de tiempo a segundos
+      const segundosTranscurridos = tiempoTranscurrido / 1000;
+
       alert('¡Felicidades! Has resuelto el rompecabezas.' + movimientos + ' movimientos.');
     }
   });
